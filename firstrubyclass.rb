86644e9fe0530f63d3f45b5461dadbc394d8f126
John Dierks <johndierks@gmail.com>
2012-01-28 15:30:47 -0700

    Added a method print_array which by default prints out array from 1 to 1000 but can also take any custom range.

--- firstrubyclass.rb
+++ firstrubyclass.rb
@@ -2,5 +2,11 @@ class FirstRubyClass
   def current_date
     require 'date'
     puts Date.today.to_s
-  end 
+  end
+  
+  def print_array(start_number = 1, end_number = 1000)
+   (start_number..end_number).each{ |x| puts x}
+  end
+  
+  
 end
