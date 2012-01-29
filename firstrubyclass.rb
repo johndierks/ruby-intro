34033502e146d7fe0f7201fa3c1926093a8147a5
John Dierks <johndierks@gmail.com>
2012-01-28 17:28:57 -0700

    added fibonacci method which will return print to the console a list of the fibonacci sequence that is as long as you specify.

--- firstrubyclass.rb
+++ firstrubyclass.rb
@@ -26,5 +26,19 @@ class FirstRubyClass
     end
   end  
   
+  def fibonacci(length)
+    fib_array = []
+    
+    for i in 0..length
+       if i == 0 then
+         fib_array[i] = 0
+       elsif i == 1 then
+         fib_array[i] = 1
+       else
+         fib_array[i] = fib_array[i-1]+fib_array[i-2]
+       end
+       puts fib_array[i]
+    end
+  end
   
 end
